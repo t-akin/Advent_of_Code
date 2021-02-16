c3bddbb86859962a5820ce868d2e74abf934f435
t-akin <toygar.akin3@gmail.com>
2021-02-17 01:08:22 +0300

    Part 2 Added for Day 2

--- 2020_02/da2.py
+++ 2020_02/da2.py
@@ -30,6 +30,9 @@ limits, char, passwords = unpack(passes)
 # correct char list
 char = list(map(lambda x: x.split(":")[0], char))
 
+### Part A ###
+##############
+
 # separate minimum and maximum values
 mins = list(map(int, map(lambda x: x.split("-")[0], limits)))
 maxes = list(map(int, map(lambda x: x.split("-")[1], limits)))
@@ -54,3 +57,28 @@ df["Boolean"] = 1*((df["mins"] <= df["occurences"]) & (df["maxes"] >= df["occure
 df.head()
 
 print("Number of valid passwords are ", str(df["Boolean"].sum()))
+
+### Part B ###
+##############
+
+# rename columns for convenience
+df = df.rename(columns = {'mins': 'firstPos', "maxes": "secondPos"})
+
+# for every row, find the characters in the password for the given positions.
+# if they match with the given char, return 1; else 0.
+# i believe python should have an easier way to do this.
+for i in range(0, len(df)):
+   fp = df["firstPos"][i]
+   sp = df["secondPos"][i]
+   
+   firstC = df["passwords"][i][fp-1]
+   secondC = df["passwords"][i][sp-1]
+   
+   df["firstPosBool"][i] = 1*(firstC == df["char"][i])
+   df["secondPosBool"][i] = 1*(secondC == df["char"][i])
+
+# sum two boolean values. if result is 1, then it means exactly one position
+# contains the given char.
+df['final'] = df.loc[:,['firstPosBool','secondPosBool']].sum(axis=1)
+
+print("Number of valid passwords are ", 1*(df["final"] == 1).sum())
